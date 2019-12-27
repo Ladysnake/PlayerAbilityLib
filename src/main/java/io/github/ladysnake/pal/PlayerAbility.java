6d647a7626c952d4cde30049c5839d23c65990a4
Pyrofab <redstoneinfire@gmail.com>
2019-12-27 03:23:12 +0100

    Add license headers

--- src/main/java/io/github/ladysnake/pal/PlayerAbility.java
+++ src/main/java/io/github/ladysnake/pal/PlayerAbility.java
@@ -1,3 +1,20 @@
+/*
+ * PlayerAbilityLib
+ * Copyright (C) 2019 Ladysnake
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; If not, see <https://www.gnu.org/licenses>.
+ */
 package io.github.ladysnake.pal;
 
 import io.github.ladysnake.pal.impl.PalInternals;
@@ -10,8 +27,8 @@ import java.util.function.BiFunction;
 
 public final class PlayerAbility {
     final Event<PlayerAbilityUpdatedCallback> updateEvent = PalInternals.createUpdateEvent();
-    final BiFunction<PlayerAbility, PlayerEntity, AbilityTracker> trackerFactory;
-    final Identifier id;
+    private final BiFunction<PlayerAbility, PlayerEntity, AbilityTracker> trackerFactory;
+    private final Identifier id;
 
     /**
      * @see Pal#registerAbility(String, String, BiFunction)
