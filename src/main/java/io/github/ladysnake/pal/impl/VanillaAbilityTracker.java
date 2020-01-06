22a136333ef9fdec287764022d2b38d7de313d39
Pyrofab <redstoneinfire@gmail.com>
2020-01-06 12:19:13 +0100

    We 2020 now bois

--- src/main/java/io/github/ladysnake/pal/impl/VanillaAbilityTracker.java
+++ src/main/java/io/github/ladysnake/pal/impl/VanillaAbilityTracker.java
@@ -1,6 +1,6 @@
 /*
  * PlayerAbilityLib
- * Copyright (C) 2019 Ladysnake
+ * Copyright (C) 2019-2020 Ladysnake
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
